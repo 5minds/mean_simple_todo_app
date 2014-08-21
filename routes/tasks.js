97ec3072d57866d6773bffaa4c52d1db46bc1886
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2014-08-21 15:29:25 +0200

    added a directive for display the tags

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,73 +7,73 @@ var Task = require('./../models/task');
 var debugError = require('debug')('tasks:error');
 
 router.get('/', function(req, res) {
-	Task.find().where('done').ne(true).select('_id title note tags done').exec(function(findError, tasks) {
+  Task.find().where('done').ne(true).select('_id title note tags done').exec(function(findError, tasks) {
 
-		if (findError) {
-			debugError("Cannot find tasks");
-			res.json(404, findError);
-		} else {
-			res.json(tasks);
-		}
+    if (findError) {
+      debugError("Cannot find tasks");
+      res.json(404, findError);
+    } else {
+      res.json(tasks);
+    }
 
-	});
+  });
 });
 
 router.get('/:id', function(req, res) {
-	Task.findById(req.params.id).select('_id title note tags done').exec(function(findError, task) {
-		
-		if (findError) {
-			debugError('Cannot find task with id "' + req.params.id + '"');
-			res.json(404);
-		} else {
-			res.json(task);
-		}
-	});
+  Task.findById(req.params.id).select('_id title note tags done').exec(function(findError, task) {
+    
+    if (findError) {
+      debugError('Cannot find task with id "' + req.params.id + '"');
+      res.json(404);
+    } else {
+      res.json(task);
+    }
+  });
 });
 
 router.post('/', function(req, res) {
 
-	var attr = _.pick(req.body, 'title', 'note', 'tags', 'done');
+  var attr = _.pick(req.body, 'title', 'note', 'tags', 'done');
 
-	var task = new Task(attr);
+  var task = new Task(attr);
 
-	task.save(function(saveError, task) {
-		
-		if (saveError) {
-			debugError(saveError);
-			res.json(422, saveError); // Unprocessable Entity
-		} else {
-			res.json(201, _.pick(task, '_id', 'title', 'note', 'tags', 'done'));
-		}
+  task.save(function(saveError, task) {
+    
+    if (saveError) {
+      debugError(saveError);
+      res.json(422, saveError); // Unprocessable Entity
+    } else {
+      res.json(201, _.pick(task, '_id', 'title', 'note', 'tags', 'done'));
+    }
 
-	});
+  });
 });
 
 router.put('/:id', function(req, res) {
-	var attr = _.pick(req.body, 'title', 'note', 'tags', 'done');
+  var attr = _.pick(req.body, 'title', 'note', 'tags', 'done');
 
-	Task.findByIdAndUpdate(req.params.id, attr).exec(function(updateError, task) {
-		
-		if (updateError) {
-			debugError(updateError);
-			req.json(422, updateError); // Unprocessable Entity
-		} else {
-			res.json(200, _.pick(task, '_id', 'title', 'note', 'tags', 'done'));
-		}
+  Task.findByIdAndUpdate(req.params.id, attr).exec(function(updateError, task) {
+    
+    if (updateError) {
+      debugError(updateError);
+      req.json(422, updateError); // Unprocessable Entity
+    } else {
+      res.json(200, _.pick(task, '_id', 'title', 'note', 'tags', 'done'));
+    }
 
-	});
+  });
 });
 
 router.delete('/:id', function(req, res) {
-	
-	Task.findByIdAndRemove(req.params.id).exec(function(deleteError) {
-		if (deleteError) {
-			debugError(deleteError);
-			res.json(404);
-		} else {
-			res.json(204); // no content
-		}
-	});
+  
+  Task.findByIdAndRemove(req.params.id).exec(function(deleteError) {
+    if (deleteError) {
+      debugError(deleteError);
+      res.json(404);
+    } else {
+      res.json(204); // no content
+    }
+  });
 
 });
 
