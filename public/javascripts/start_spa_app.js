f7bbdc855ff7eb0084b45d4c978003bdf9b5f1c6
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2014-08-28 16:49:49 +0200

    change the server url for the client

--- public/javascripts/start_spa_app.js
+++ public/javascripts/start_spa_app.js
@@ -6,5 +6,5 @@ var simpleToDoApp = angular.module('simpleToDoApp', [
   'underscore'
   ]);
 
-simpleToDoApp.constant('AppConfig', {API_HOST: 'http://moellenbeck-t.tunnlr.com//'});
-//simpleToDoApp.constant('AppConfig', {API_HOST: 'http://localhost:3000/'});
+//simpleToDoApp.constant('AppConfig', {API_HOST: 'http://moellenbeck-t.tunnlr.com//'});
+simpleToDoApp.constant('AppConfig', {API_HOST: 'http://localhost:3000/'});
