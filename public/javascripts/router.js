014fdfc1e74eb723d55965128a41d76688661202
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2015-03-25 18:32:00 +0100

    update to angular 1.4.x and using of assets hosts for templates

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,6 +1,15 @@
 'use strict';
 
-var editTemlateUrl = 'partials/tasks/edit.html';
+var editTemlateUrl = 'http://localhost:3001/partials/tasks/edit.html';
+
+angular.module('simpleToDoApp').config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'http://localhost:3001/**'
+  ]);
+});
 
 angular.module('simpleToDoApp').config(['$routeProvider',
   function($routeProvider) {
--- public/javascripts/start_spa_app.js
+++ public/javascripts/start_spa_app.js
@@ -9,3 +9,4 @@ var simpleToDoApp = angular.module('simpleToDoApp', [
 //simpleToDoApp.constant('AppConfig', {API_HOST: 'http://moellenbeck-t.tunnlr.com//'});
 //simpleToDoApp.constant('AppConfig', {API_HOST: 'http://localhost:3000/'});
 simpleToDoApp.constant('AppConfig', {API_HOST: 'http://www.mindassist.net:8080/'});
+
