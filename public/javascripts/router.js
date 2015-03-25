d0d69ff2a7b562d9af9bb8a25c1fc0ed7b764a81
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2015-03-25 19:40:24 +0100

    added some hints

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,30 +1,36 @@
 'use strict';
 
-var editTemlateUrl = 'http://localhost:3001/partials/tasks/edit.html';
+function configEdit(editUrl) {
+  //var editTemlateUrl = 'http://localhost:3001/partials/tasks/edit.html';
 
-angular.module('simpleToDoApp').config(function($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    // Allow same origin resource loads.
-    'self',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'http://localhost:3001/**'
+  angular.module('simpleToDoApp').config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      // Allow loading from our assets domain.  Notice the difference between * and **.
+      editUrl
+    ]);
+  });
+
+  angular.module('simpleToDoApp').config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/', {
+          templateUrl: 'partials/tasks/index.html',
+          controller: 'TaskIndexCtrl'
+        }).
+        when('/new', {
+          templateUrl: 'partials/tasks/new.html',
+          controller: 'TaskNewCtrl'
+        }).
+        when('/edit/:taskId', {
+          templateUrl: editUrl,
+          controller: 'TaskEditCtrl'
+        });
+    }
   ]);
-});
 
-angular.module('simpleToDoApp').config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/tasks/index.html',
-        controller: 'TaskIndexCtrl'
-      }).
-      when('/new', {
-        templateUrl: 'partials/tasks/new.html',
-        controller: 'TaskNewCtrl'
-      }).
-      when('/edit/:taskId', {
-        templateUrl: editTemlateUrl,
-        controller: 'TaskEditCtrl'
-      });
-  }
-]);
+}
+
+configEdit('http://localhost:3001/partials/tasks/edit.html');
+
